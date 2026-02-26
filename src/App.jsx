ba74fb20383e04eef52a53954b41ba24bc2dd772
Yujin Cho <121141298+biddenboi@users.noreply.github.com>
2026-02-26 09:54:16 -0600

    Improved code documentation for App.jsx & App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,70 @@
+import { useState, createContext, useEffect, useMemo } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+
+import './App.css';
 import Dashboard from "./Pages/Dashboard/Dashboard";
 import Events from "./Pages/Events/Events";
 import Settings from "./Pages/Settings/Settings";
 import Journal from "./Pages/Journal/Journal";
 import Profile from "./Pages/Profile/Profile";
 import Shop from "./Pages/Shop/Shop";
-
-import './App.css';
-import { useState, createContext, useEffect, useMemo } from 'react';
 import DatabaseConnection from "./network/DatabaseConnection";
 import PlayerDatabase from "./network/Database/PlayerDatabase";
-import { Routes, Route, useNavigate } from 'react-router-dom';
 
 export const DatabaseConnectionContext = createContext();
 
+/*Start Point of React Program. Handles page navigation and database connection.*/
 function App() {
+  /*Internal Data*/
   const nav = useNavigate(); 
 
-  const [inTaskSession, setInTaskSession] = useState(false);
+  const [isTaskSession, setIsTaskSession] = useState(false);
   
-  const databaseConnection = useMemo(() => new DatabaseConnection(), []);
-  const playerDatabase = useMemo(
-    () => new PlayerDatabase(databaseConnection),
+  const databaseConnection = useMemo(() => new DatabaseConnection(), 
+    []
+  );
+
+  //dependent on databaseConnection so it recalls once databaseConnection is established.
+  const playerDatabase = useMemo(() => new PlayerDatabase(databaseConnection),
     [databaseConnection]
   );
-  
 
-  //maybe improve the function name. Checks if task is in session and changes page if not.
+  // calls createPlayer on app load, if player does not exist then it creates a new profile.
+  useEffect(() => {
+    const tryNewProfile = async () => {
+      const player = {
+        username: "Guest",
+        createdAt: new Date().toISOString().split('T')[0]
+      }
+      await playerDatabase.createPlayer(player);
+    }
+
+    tryNewProfile();
+  }, [playerDatabase])
+
+  //navigating across routes
   const navigate = (route) => {
     if (!inTaskSession) {
       nav(route);
     }
   }
-
-  useEffect(() => {
-    //FINISH PUSHING PLAYER DATA  
-    const createPlayer = async () => {
-      const player = {
-        username: "Guest",
-        createdAt: new Date().toISOString(),
-        localCreatedAt: new Date().toLocaleString('sv').split(' ')[0]
-      }
-      await playerDatabase.createPlayer(player);
-    }
-
-    createPlayer();
-  }, [playerDatabase])
-
   
   return <>
-    <div className={inTaskSession ? "navigation-bar task-in-session" : "navigation-bar"}>
+    <div className={isTaskSession ? "navigation-bar task-in-session" : "navigation-bar"}>
       <a onClick={() => navigate("/")}>Dashboard</a>
       <a onClick={() => navigate("/events")}>Events</a>
       <a onClick={() => navigate("/shop")}>Shop</a>
       <a onClick={() => navigate("/journal")}>Journal</a>
       <a onClick={() => navigate("/settings")}>Settings</a>
     </div>
+
+    {{/*Provides database connection to all child components.*/}}
     <DatabaseConnectionContext.Provider value={databaseConnection}>
         <Routes>
-          <Route path='/' element={<Dashboard inTaskSession={inTaskSession} setInTaskSession={setInTaskSession}></Dashboard>}/>
+          <Route 
+            path='/' 
+            element={<Dashboard isTaskSession={isTaskSession} setIsTaskSession={setIsTaskSession}
+            index></Dashboard>}/>
           <Route path='/events' element={<Events></Events>}/>
           <Route path='/shop' element={<Shop></Shop>}/>
           <Route path='/journal' element={<Journal></Journal>}/>
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+//Note: possibly move BrowserRouter to App.jsx
 import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
